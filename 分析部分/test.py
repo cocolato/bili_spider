faf91b65b2a5f9d2eebff9a62274bd555c0788a5
zz54165514 <zhuhaiswjtu@qq.com>
2019-11-29 00:56:46 +0800

    修改

--- 分析部分/test.py
+++ 分析部分/test.py
@@ -5,15 +5,35 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import LSTM, GRU
+from RNN_model import RNN
 
 
 def build_model():
     model = Sequential()
-    model.add(LSTM(units=4, input_shape=(1, 1)))
+    model.add(GRU(units=4, input_shape=(1, 1)))
     model.add(Dense(units=1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
+
+
+seed = 7
+batch_size = 1
+epochs = 10
+filename = 'international-airline-passengers.csv'
+footer = 3
+look_back = 1
+
+
+def create_dataset(dataset):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - 1):
+        x = dataset[i: i + look_back, 0]
+        dataX.append(x)
+        y = dataset[i + look_back, 0]
+        dataY.append(y)
+        # print('X: %s, Y: %s' % (x, y))
+    return np.array(dataX), np.array(dataY)
 
 
 client = MongoClient('mongodb://localhost:27017')
@@ -22,11 +42,17 @@ p = Processor(dbs['53766576'])
 score = p.get_score()
 array = p.get_array()
 incre = p.get_increment()
-scaler = MinMaxScaler()
-dataset = p.run_pca()[:, 1]
-dataset = dataset.reshape(-1, 1)
-print(dataset)
-train_X = dataset
-train_Y = score[1:]
-model = build_model()
-model.fit(train_X, train_Y, epochs=100, batch_size=1)
+dataset = p.run_pca()[:, 0]
+gru_model = RNN(dataset=dataset, input_shape=(1, 1))
+gru_model.normalize()
+print(gru_model.create_train_data(train_prop=0.67))
+
+# print(dataset)
+# dataset = dataset.reshape(-1, 1)
+# X_train, y_train = create_dataset(dataset)
+# X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+#
+#
+# model = build_model()
+#
+# model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=2)
