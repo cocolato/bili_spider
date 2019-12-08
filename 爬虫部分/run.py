d97456be666b332b6cbd6e83fd0612a3eca16478
zz54165514 <zhuhaiswjtu@qq.com>
2019-12-08 16:23:37 +0800

    修改

--- 爬虫部分/run.py
+++ 爬虫部分/run.py
@@ -3,7 +3,7 @@ from get_data import get_data_run
 
 if __name__ == "__main__":
 
-    schedule.every(5).minutes.do(get_data_run, 2019, 11, 19)
+    schedule.every(15).minutes.do(get_data_run, 2019, 11, 19)
 
     while True:
         schedule.run_pending()
